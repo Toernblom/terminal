0d1b3925d3b64d2888fbd37413c3793802997619
Anton <49762405+Toernblom@users.noreply.github.com>
2022-10-19 09:15:41 +0200

    Added plugins

--- setup.sh
+++ setup.sh
@@ -5,7 +5,11 @@ chsh -s $(which zsh)
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 sudo apt-get install fonts-powerline
 sed 's/robbyrussell/agnoster/g' ~/.zshrc
+sed 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
 
+source ~/.zshrc
